13f08d05af88b617c8cc7ef90eb77cc3de73cbe7
wilypomegranate <wilypomegranate@users.noreply.github.com>
2019-07-21 20:45:08 -0400

    WIP.

--- src/bin/arp_whoami.rs
+++ src/bin/arp_whoami.rs
@@ -1,11 +1,11 @@
 extern crate netrs;
 
-use netrs::hw::Interface;
 use netrs::hw::socket::RawSocket;
+use netrs::hw::Interface;
 
 pub fn main() {
-    let socket: RawSocket = RawSocket{};
+    let socket: RawSocket = RawSocket::new().expect("Socket error.");
     const MTU: usize = 1500;
-    let mut buffer: [u8;MTU] = [0;MTU];
+    let mut buffer: [u8; MTU] = [0; MTU];
     socket.recv(&mut buffer);
 }
--- src/hw/mod.rs
+++ src/hw/mod.rs
@@ -1,4 +1,5 @@
 extern crate libc;
+use libc::c_int;
 
 pub trait Interface {
     fn recv(&self, data: &mut [u8]);
@@ -6,8 +7,15 @@ pub trait Interface {
 }
 
 pub type Result<T> = std::result::Result<T, HwError>;
+
 #[derive(Debug, Clone)]
-struct HwError;
+pub struct HwError {
+    errno: c_int,
+}
+
+// impl std::error::Error for HwError {
+//     fn description(&self) -> &str {}
+// }
 
 #[cfg(target_os = "linux")]
 pub mod socket {
@@ -15,26 +23,35 @@ pub mod socket {
     use libc::{c_int, c_void, ssize_t};
 
     pub struct RawSocket {
-        socket: i32
+        socket: i32,
     }
 
-    // impl RawSocket {
-    //     pub fn new() -> Result<RawSocket, super::HwError> {
-    //     }
-    // }
+    impl RawSocket {
+        pub fn new() -> Result<Self, super::HwError> {
+            let eth: u16 = libc::ETH_P_ALL as u16;
+            unsafe {
+                let socket: c_int =
+                    libc::socket(libc::AF_PACKET, libc::SOCK_RAW, eth.to_be() as i32);
+                if socket != -1 {
+                    return Ok(RawSocket { socket });
+                } else {
+                    Err(super::HwError {
+                        errno: *libc::__errno_location(),
+                    })
+                }
+            }
+        }
+    }
 
     impl super::Interface for RawSocket {
         fn recv(&self, data: &mut [u8]) {
-            let sock: c_int = -1;
-            let eth: u16 = libc::ETH_P_ALL as u16;
             let bytes_received: ssize_t = 0;
             unsafe {
-                let sock: c_int = libc::socket(libc::AF_PACKET, libc::SOCK_RAW, eth.to_be() as i32);
                 const SOCK_FLAGS: c_int = 0;
-                if sock != -1 {
+                if self.socket != -1 {
                     // let buf: *mut c_void = data as *mut c_void;
                     let bytes_received: ssize_t = libc::recv(
-                        sock,
+                        self.socket,
                         data.as_mut_ptr() as *mut c_void,
                         data.len(),
                         SOCK_FLAGS,
