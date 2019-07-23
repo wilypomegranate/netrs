13943bc069d040f8c2d255adbd2cce197a554c5f
wilypomegranate <wilypomegranate@users.noreply.github.com>
2019-07-22 22:26:41 -0400

    WIP. Doing some benchmarking as well.

--- src/bin/arp_whoami.rs
+++ src/bin/arp_whoami.rs
@@ -1,11 +1,31 @@
 extern crate netrs;
 
+use netrs::hw::pcap::PcapReader;
 use netrs::hw::socket::RawSocket;
 use netrs::hw::Interface;
+use std::env;
 
 pub fn main() {
-    let socket: RawSocket = RawSocket::new().expect("Socket error.");
-    const MTU: usize = 1500;
-    let mut buffer: [u8; MTU] = [0; MTU];
-    socket.recv(&mut buffer);
+    // let socket: RawSocket = RawSocket::new().expect("Socket error.");
+    // const MTU: usize = 1500;
+    // let mut buffer: [u8; MTU] = [0; MTU];
+    // socket.recv(&mut buffer);
+
+    let args: Vec<String> = env::args().collect();
+
+    let mut pcap: PcapReader = PcapReader::new(&args[1]).expect("Pcap reader error.");
+    let mut buffer = vec![0; pcap.header.snaplen as usize];
+
+    let mut count: usize = 0;
+    while let Ok(record) = pcap.read_record() {
+        // println!(
+        //     "sec: {}, usec: {}, incl_len: {}, orig_len: {}",
+        //     record.ts_sec, record.ts_usec, record.incl_len, record.orig_len
+        // );
+        pcap.read_data(&record, &mut buffer).unwrap();
+        count += 1
+    }
+    println!("Count {}", count);
+
+    // println!("Magic number: {} snaplen: {}.", pcap.header.magic_number, pcap.header.snaplen);
 }
--- src/hw/mod.rs
+++ src/hw/mod.rs
@@ -2,7 +2,7 @@ extern crate libc;
 use libc::c_int;
 
 pub trait Interface {
-    fn recv(&self, data: &mut [u8]);
+    fn recv(&self, data: &mut [u8]) -> usize;
     // fn send(data: &[u8]);
 }
 
@@ -44,7 +44,7 @@ pub mod socket {
     }
 
     impl super::Interface for RawSocket {
-        fn recv(&self, data: &mut [u8]) {
+        fn recv(&self, data: &mut [u8]) -> usize {
             let bytes_received: ssize_t = 0;
             unsafe {
                 const SOCK_FLAGS: c_int = 0;
@@ -58,14 +58,147 @@ pub mod socket {
                     );
                     if bytes_received > 0 {
                         println!("Received {}.", bytes_received);
+                        bytes_received as usize
                     } else {
                         println!("Error2");
+                        0
                     }
                 } else {
                     println!("Error");
+                    0
                 }
             }
         }
     }
 
+}
+
+pub mod pcap {
+
+    use byteorder::{BigEndian, LittleEndian, NativeEndian, ReadBytesExt};
+    use std::fs::File;
+    use std::io::prelude::*;
+    use std::io::BufReader;
+
+    const PCAP_MAGIC: u32 = 0xa1b2c3d4;
+
+    pub struct PcapHeader {
+        pub magic_number: u32,
+        version_major: u16,
+        version_minor: u16,
+        thiszone: i32,
+        sigfigs: u32,
+        pub snaplen: u32,
+        network: u32,
+    }
+
+    pub struct PcapRecord {
+        pub ts_sec: u32,
+        pub ts_usec: u32,
+        pub incl_len: u32,
+        pub orig_len: u32,
+    }
+
+    pub struct PcapReader {
+        pub header: PcapHeader,
+        reader: BufReader<File>,
+        swap: bool,
+    }
+
+    impl PcapReader {
+        pub fn new(file: &str) -> std::io::Result<Self> {
+            // let file = File::open(file);
+            let mut reader = BufReader::new(File::open(file)?);
+            let (header, swap) = PcapReader::read_header(&mut reader)?;
+            let pcap_reader = PcapReader {
+                header,
+                reader,
+                swap,
+            };
+            Ok(pcap_reader)
+        }
+
+        fn parse_header(
+            reader: &mut BufReader<File>,
+            magic_number: u32,
+            swap: bool,
+        ) -> std::io::Result<(PcapHeader, bool)> {
+            match swap {
+                false => Ok((
+                    PcapHeader {
+                        magic_number: magic_number,
+                        version_major: reader.read_u16::<NativeEndian>()?,
+                        version_minor: reader.read_u16::<NativeEndian>()?,
+                        thiszone: reader.read_i32::<NativeEndian>()?,
+                        sigfigs: reader.read_u32::<NativeEndian>()?,
+                        snaplen: reader.read_u32::<NativeEndian>()?,
+                        network: reader.read_u32::<NativeEndian>()?,
+                    },
+                    swap,
+                )),
+
+                true => Ok((
+                    PcapHeader {
+                        magic_number: u32::swap_bytes(magic_number),
+                        version_major: u16::swap_bytes(reader.read_u16::<NativeEndian>()?),
+                        version_minor: u16::swap_bytes(reader.read_u16::<NativeEndian>()?),
+                        thiszone: i32::swap_bytes(reader.read_i32::<NativeEndian>()?),
+                        sigfigs: u32::swap_bytes(reader.read_u32::<NativeEndian>()?),
+                        snaplen: u32::swap_bytes(reader.read_u32::<NativeEndian>()?),
+                        network: u32::swap_bytes(reader.read_u32::<NativeEndian>()?),
+                    },
+                    swap,
+                )),
+            }
+        }
+
+        fn read_header(reader: &mut BufReader<File>) -> std::io::Result<(PcapHeader, bool)> {
+            let magic_number = reader.read_u32::<NativeEndian>().unwrap();
+
+            // Correct endianness.
+            if magic_number == PCAP_MAGIC {
+                PcapReader::parse_header(reader, magic_number, false)
+            }
+            // Wrong endianess.
+            else if u32::swap_bytes(magic_number) == PCAP_MAGIC {
+                PcapReader::parse_header(reader, magic_number, true)
+            }
+            // Bad magic number.
+            else {
+                Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    "Bad pcap magic number.",
+                ))
+            }
+        }
+
+        pub fn read_record(&mut self) -> std::io::Result<PcapRecord> {
+            match self.swap {
+                false => Ok(PcapRecord {
+                    ts_sec: self.reader.read_u32::<NativeEndian>()?,
+                    ts_usec: self.reader.read_u32::<NativeEndian>()?,
+                    incl_len: self.reader.read_u32::<NativeEndian>()?,
+                    orig_len: self.reader.read_u32::<NativeEndian>()?,
+                }),
+
+                true => Ok(PcapRecord {
+                    ts_sec: u32::swap_bytes(self.reader.read_u32::<NativeEndian>()?),
+                    ts_usec: u32::swap_bytes(self.reader.read_u32::<NativeEndian>()?),
+                    incl_len: u32::swap_bytes(self.reader.read_u32::<NativeEndian>()?),
+                    orig_len: u32::swap_bytes(self.reader.read_u32::<NativeEndian>()?),
+                }),
+            }
+        }
+
+        pub fn read_data(&mut self, record: &PcapRecord, data: &mut [u8]) -> std::io::Result<()> {
+            self.reader.read_exact(&mut data[0..record.incl_len as usize])
+        }
+
+    }
+
+    // impl super::Interface for PcapReader {
+    //     fn recv(&self, data: &mut [u8]) -> usize {
+    //         0
+    //     }
+    // }
 }
