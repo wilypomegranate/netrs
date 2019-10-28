e1b014bf365693425bdae66884179447efaab5bd
wilypomegranate <wilypomegranate@users.noreply.github.com>
2019-10-27 22:17:19 -0400

    Starting mac impl.

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -44,30 +44,59 @@ pub mod ethernet {
     }
 }
 
+pub mod mac {
+
+    pub struct MacAddress<'a> {
+        addr: &'a [u8],
+    }
+
+    impl<'a> MacAddress<'a> {
+        pub fn new(data: &'a [u8]) -> MacAddress<'a> {
+            MacAddress { addr: data}
+        }
+    }
+}
+
 pub mod arp {
+    use crate::net::mac::MacAddress;
     use byteorder::{ByteOrder, NativeEndian};
 
     pub struct Arp<'a> {
         data: &'a [u8],
     }
 
-    pub fn htype() -> u16 {
-        0
-    }
+    impl<'a> Arp<'a> {
+        pub fn htype() -> u16 {
+            0
+        }
 
-    pub fn ptype() -> u16 {
-        0
-    }
+        pub fn ptype() -> u16 {
+            0
+        }
 
-    pub fn hlen() -> u8 {
-        0
-    }
+        pub fn hlen() -> u8 {
+            0
+        }
 
-    pub fn plen() -> u8 {
-        0
-    }
+        pub fn plen() -> u8 {
+            0
+        }
 
-    pub fn operation() -> u16 {
-        0
+        pub fn oper() -> u16 {
+            0
+        }
+
+        pub fn sha(&self) -> MacAddress<'a> {
+            MacAddress::new(&self.data[8..14])
+        }
+
+        // pub fn spa(&self) -> Ipv4Address<'a> {
+        // }
+
+        pub fn tha(&self) -> MacAddress<'a> {
+            MacAddress::new(&self.data[18..24])
+        }
+
+        // pub fn tpa(&self) -> Ipv4Address<'a> {}
     }
 }
